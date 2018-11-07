213a9b64977b1698b3d201efe76ca562dfcd6fd3
yuy1-kpit <44699570+yuy1-kpit@users.noreply.github.com>
2018-11-07 12:18:10 +0100

    Add files via upload

---
+++ CI_demo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 11:57:35 2018
+
+@author: yuy1
+"""
+
+def inc(inputNum):
+    return inputNum + 1
+print(inc(10))
+
---
+++ demo.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 12:06:41 2018
+
+@author: yuy1
+"""
+
+import unittest
+from CI_demo import inc
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(inc(0),1)
+        self.assertEqual(inc(100),101)
+        self.assertEqual(inc(1), 2)
+        self.assertEqual(inc(-1), 0)
+        self.assertEqual(inc(4), 4)
+        
+        
+if __name__=='__main__':
+    unittest.main()
